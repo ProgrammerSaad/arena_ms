5b9024eb969f18c53e9c75ebd492ab577299ae83
ProgrammerSaad <hoomik12@wp.pl>
2018-06-11 21:34:33 +0200

    Michał zmiana 18.01.2018

---
+++ src/main/java/com/example/arena/model/postaci/RodzajAtakuEnum.java
@@ -0,0 +1,13 @@
+package com.example.arena.model.postaci;
+
+/**
+ * Created by Saad on 2018-03-02.
+ */
+public enum RodzajAtakuEnum {
+
+    UDERZENIE,
+    STRZAL,
+    UGRYZIENIE,
+    CZAR;
+
+}
